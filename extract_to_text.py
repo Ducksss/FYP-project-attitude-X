b759b26be8ef977ad10363fdcbc6aa8e60daad00
27thRay <raymondng009@gmail.com>
2023-10-18 16:56:46 +0800

    Tried experimenting with resume parsers

---
+++ extract_to_text.py
@@ -0,0 +1,9 @@
+import docx2txt
+from pdfminer.high_level import extract_text
+
+#Functions for conversion
+def convertPDFToText(file_path):
+    return extract_text(file_path)
+
+def convertDocxToText(file_path):
+    return docx2txt.process(file_path)
