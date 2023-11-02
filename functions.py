9bc6be89767fd342bcea3f2cca9586c887eb617b
27thRay <raymondng009@gmail.com>
2023-11-02 16:13:25 +0800

    Added login page

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
 import os
+from stopwords import filter_stopwords
+from ner import jd_prompt_1, resume_prompt, convert_to_dict
+from extract_from_text import extract_email, extract_contact_number
+from similarity_matching import get_similarity_score
 
 # Function to check file extension
 def check_file_type(file_path):
@@ -9,3 +13,33 @@ def check_file_type(file_path):
         return "PDF"
     else:
         return "Other"
+    
+def process_text(text, filetype):
+    text_filter = filter_stopwords(text)
+    if filetype == 'JD':
+        #Getting Dictionary of details from Job Description
+        jd_result = jd_prompt_1(text_filter)
+        jd_dict = convert_to_dict(jd_result)
+        return jd_dict
+    elif filetype == 'Resume':
+        #Getting Dictionary of details from resume
+        contact_number = extract_contact_number(text)
+        email = extract_email(text)
+        resume_result = resume_prompt(text_filter)
+        resume_dict = convert_to_dict(resume_result)
+        resume_dict['contact_number'] = contact_number
+        resume_dict['email'] = email
+        return resume_dict
+    
+def print_score(jd_dict, resume_dict):
+    techsk_score, softsk_score, lang_score = get_similarity_score(jd_dict, resume_dict)
+    overall_score = techsk_score*0.4+softsk_score*0.4+lang_score*0.2
+    
+    score_string = "Scoring for " + resume_dict['Name'] + "\n" + \
+                'Technical Skills Score: ' + techsk_score + "\n" + \
+                'Soft Skills Score: ' + softsk_score + "\n" + \
+                'Language Skills Score: ' + lang_score + "\n" + \
+                'Overall Skills Score: ' + str(overall_score) + "\n"
+
+                    
+    return score_string
---
+++ pages/1_login.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from PIL import Image
+import streamlit_authenticator as stauth
+
+image = Image.open('static/drawmetricslogo.jpg')
+
+
+import yaml
+from yaml.loader import SafeLoader
+    # Loading config file
+with open('authentication/config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+    # Creating the authenticator object
+authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'], 
+        config['cookie']['key'], 
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
+
+col1, col2, col3 = st.columns([1.3, 4, 1.3])
+col2.image(image, use_column_width=True)
+
+authenticator.login('Login', 'main')
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
