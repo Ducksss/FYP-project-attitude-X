f54b767e6d38335e8855ee9f163bd221d29647a6
27thRay <raymondng009@gmail.com>
2023-11-02 09:45:36 +0800

    Added BERT Similarity score

--- extract_from_text.py
+++ extract_from_text.py
@@ -1,26 +1,5 @@
 ##Import NER Libraries
 import re
-import spacy
-# from spacy.lang.en.stop_words import STOP_WORDS
-import vercel_ai
-import time
-client = vercel_ai.Client()
-
-
-def extract_name(text):
-    nlp = spacy.load('en_core_web_trf')                                                                                                                  
-    sents = nlp(text) 
-    people = [ee for ee in sents.ents if ee.label_ == 'PERSON']       
-    # Remove newline characters and split the text into lines
-    name = str(people[0])
-
-    # Split the text by newline characters and take the first part
-    # some names may be falsely extracted to the next line
-    corrected_name = name.split("\n")[0]
-
-    # Split the text into words, convert each word to lowercase with the first letter capitalized
-    formatted_name = ' '.join(word.capitalize() for word in corrected_name.split())
-    return formatted_name, people
 
 def extract_email(text):
     email = None
@@ -50,26 +29,3 @@ def extract_contact_number(text):
     formatted_contact_number = contact_number.replace(" ", "")
 
     return formatted_contact_number
-
-def ner_extraction(text):
-        prompt = f'''
-                Resume:
-                {text}'''
-
-        prompt_template = f'''
-        Extract the name, technical skills, soft skills and languages only from the resume, do not do more than what was asked:
-        {prompt}'''
-
-        while True:
-            try:
-                for chunk in client.generate("replicate:replicate/llama-2-70b-chat", prompt_template, params = {
-                "replicate:replicate/llama-2-70b-chat": {
-                    "temperature": 0.75,
-                    "maximumLength": 1000,
-                    "topP": 1,
-                    "repetitionPenalty": 1
-                }}):
-                    print(chunk, end="", flush=True)
-                break
-            except:
-                pass
--- script.py
+++ script.py
@@ -1,54 +1,78 @@
 ##Importing libraries
 import os
 
-
 ##Importing Functions
 from functions import check_file_type
 from convert_to_text import convertPDFToText, convertDocxToText
-from extract_from_text import extract_name, extract_email, extract_contact_number, ner_extraction
-
-#Provide the Resume file path as an argument
-file_path = "resume_data/Resume_1.docx"  
-#Check file type
-file_type = check_file_type(file_path)
-#Check file size
-size = os.path.getsize(file_path)
-
-
-#Text Extracting for file types and file size
-if size>10000000:#10 Million Bytes for 10MB
-    print('*************File size too large, please compress file or try again!*************')
-else:
-    #extract text for pdf
-    if file_type == "PDF":    
-        resume_text = convertPDFToText(file_path)
-        #print("*************Successfully extracted PDF*************")
-        #print(text)
-    elif file_type == "DOCX": #10 Million Bytes for 10MB
-        resume_text = convertDocxToText(file_path)
-        #print("*************Successfully extracted Word File*************")
-        #print(text)
+from extract_from_text import extract_email, extract_contact_number
+from ner import jd_prompt_1, resume_prompt, convert_to_dict
+from similarity_matching import get_similarity_score
+  
+def file_processing(file_path):
+    #Check file type
+    file_type = check_file_type(file_path)
+    #Check file size
+    size = os.path.getsize(file_path)
+    
+    #Text Extracting for file types and file size
+    if size>10000000:#10 Million Bytes for 10MB
+        print('*************File size too large, please compress file or try again!*************')
     else:
-        #File type is not accepted
-        raise Exception("*************Filetype is not accepted*************")
+        #extract text for pdf
+        if file_type == "PDF":    
+            text = convertPDFToText(file_path)
+        elif file_type == "DOCX": #10 Million Bytes for 10MB
+            text = convertDocxToText(file_path)
+        else:
+            #File type is not accepted
+            raise Exception("*************Filetype is not accepted*************")
+        
+    return text
 
 if __name__ == '__main__':
+    #Provide the Job Description & Resume file path as an argument
+    file_path_job = "sample_data/JD.docx"
+    file_path_resume = "sample_data/Resume_4.docx"
+    
+    #Extract text for resume and job description
+    jd_text = file_processing(file_path_job)
+    resume_text = file_processing(file_path_resume)
+    
+    #Getting Dictionary of details from Job Description
+    jd_result = jd_prompt_1(jd_text)
+    jd_dict = convert_to_dict(jd_result)
+    print(f'*****************Job Description: {jd_dict}')
+    
+    #Getting Dictionary of details from resume
     contact_number = extract_contact_number(resume_text)
-    if contact_number:
-        print("Contact Number:", contact_number)
-    else:
-        print("Contact Number not found")
-
     email = extract_email(resume_text)
-    if email:
-        print("Email:", email)
-    else:
-        print("Email not found")
-    name, people_list = extract_name(resume_text)
-    if name:
-        print("Name:", name)
-    else:
-        print("Name not found")
+    resume_result = resume_prompt(resume_text)
+    resume_dict = convert_to_dict(resume_result)
+    resume_dict['contact_number'] = contact_number
+    resume_dict['email'] = email
+    print(f'*****************Resume Description: {resume_dict}')
+    
+    #Compare Job Description and Resume 
+    techsk_score, softsk_score, lang_score = get_similarity_score(jd_dict, resume_dict)
+    overall_score = techsk_score*0.4+softsk_score*0.4+lang_score*0.2
+    
+    print(f"Scoring for {resume_dict['Name']}")
+    print(f'Technical Skills Score: {techsk_score}')
+    print(f'Soft Skills Score: {softsk_score}')
+    print(f'Language Skills Score: {lang_score}')
+    print(f'Overall Skills Score:{overall_score}')
+    
+    
+    #No. 1 Charlotte
+    #0.49, 0.6 (overall language has problem)
+    #No. 2 Bertrand
+    #0.46, 0.47
+    #No. 3 Mykola
+    #0.50, 0.86
+    #No. 4 Djamila
+    #0.46, 0.56
+    #No. 5 Lu zhang 
+    #0.41, 0.84
+    
 
-# remove_stop_words(resume_text, people_list, email, contact_number)
-ner_extraction(resume_text)
+    
