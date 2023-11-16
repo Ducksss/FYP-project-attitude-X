2a15d03c6e77342fca7c57c4107945d90f2d691a
27thRay <raymondng009@gmail.com>
2023-11-16 12:03:58 +0800

    Added options for local host and database in database.py

--- database.py
+++ database.py
@@ -4,9 +4,9 @@ import pandas as pd
 import streamlit as st
 
 #local host
-mongo_url = "mongodb://localhost:27017"
+# mongo_url = "mongodb://localhost:27017"
 #docker 
-# mongo_url = "mongodb://database:27017"
+mongo_url = "mongodb://database:27017"
 
 myclient = pymongo.MongoClient(mongo_url)
 mydb = myclient["resumeDB"]
